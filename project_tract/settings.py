9d997a93d8f30e83bc3aad8aafd2d13648f06268
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-31 00:26:25 +0300

    Add migrations

--- project_tract/settings.py
+++ project_tract/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 # import cloudinary
 # import cloudinary.uploader
 # import cloudinary.api
@@ -29,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # )
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
