f6f806cfec07e35b6a99aea4ca0cc0dc4b99c905
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-31 03:43:52 +0300

    Add like feature

--- project_tract/settings.py
+++ project_tract/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
-# import cloudinary
-# import cloudinary.uploader
-# import cloudinary.api
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-# cloudinary.config( 
-#   cloud_name = "dlntyyesm", 
-#   api_key = "627375511792298", 
-#   api_secret = "KB5_YDQpmi4LSbmZ7fDuwz4NsR0" 
-# )
+cloudinary.config( 
+  cloud_name = "dlntyyesm", 
+  api_key = "627375511792298", 
+  api_secret = "KB5_YDQpmi4LSbmZ7fDuwz4NsR0" 
+)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'comments',
     'like',
     'track',
-    'rest_framework'
+    'rest_framework',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
--- projects/models.py
+++ projects/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Project(models.Model):
-    image= models.ImageField()
+    image= CloudinaryField('image')
     description = models.CharField(max_length=30)
     technologies = models.CharField(max_length=500)
     github_link = models.URLField()
     date_created = models.DateField(auto_now_add=True)
     date_updated= models.DateField(auto_now_add=True)
+    # liked = models.ManyToManyField(UserAccount, null=True, blank=True, related_name='liked')
+
+    # @property
+    # def num_likes(self):
+    #     return self.liked.all().count()
 
     @classmethod
     def search_by_title(cls,search_term):
