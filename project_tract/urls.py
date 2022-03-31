6171e9d57612ac537655ec5c3ad6d3d7c0da011b
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-31 03:31:52 +0300

    Add final url adjustments

--- project_tract/urls.py
+++ project_tract/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('track-api/', include('track.urls')),
-    path('projects/', include('projects.urls'))
+    path('', include('projects.urls'))
 ]
