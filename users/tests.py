62364ef6febf6eef45e7267dd86b4c67e276749f
trevin-livele <liveletrevin6@gmail.com>
2022-04-01 17:41:46 +0300

    completed test for user

--- users/tests.py
+++ users/tests.py
@@ -26,3 +26,27 @@ class PublicUserApiTests(TestCase):
         }
 
         res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password',res.data)
+
+
+    def test_user_exists(self):
+        """test creating a user that already exists fails"""
+        payload = {'email' : 'test@gmail.com', 'password' : 'testpass' }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """test that the password must be more than 5 characters"""
+        payload = {'email': 'test@gmail.com', 'password' : 'pass'}
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        self.assertFalse(user_exists)
