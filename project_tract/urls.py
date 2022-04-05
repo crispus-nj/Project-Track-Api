f84131e7c5fa4da897e3ed7555f408dbbbdd4448
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-05 09:57:39 +0300

    changed development

--- project_tract/urls.py
+++ project_tract/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path('like-api/', include('like.urls')),
     path('api/users/', include('users.urls')),
 ]
-
-
-
-
