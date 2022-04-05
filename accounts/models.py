4fbd178ce1dad4fb6d8347a301f66a6d5beb1732
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-04-05 10:06:18 +0300

    Add merged changes

--- accounts/models.py
+++ accounts/models.py
@@ -17,14 +17,9 @@ class MyAccountManager(BaseUserManager):
             last_name = last_name,
         )
 
-
-
         user.set_password(password)
         user.save(using=self._db)
         return user
-
-
-
 
     def create_superuser(self, first_name, last_name, email, username, password):
         user = self.create_user(
