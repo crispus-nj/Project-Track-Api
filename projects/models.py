aa95d5ff04b4ad2e674bc8fb088e582e7e68e658
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-06 11:49:12 +0300

    updated track model by ading track selection column, added image column in the track model

--- projects/models.py
+++ projects/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from accounts.models import Account
+from track.models import Track
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=50)
     image= CloudinaryField('image')
     description = models.CharField(max_length=30)
     technologies = models.CharField(max_length=500)
     github_link = models.URLField()
-    date_created = models.DateField(auto_now_add=True)
-    date_updated= models.DateField(auto_now_add=True)
+    track = models.ForeignKey(Track, on_delete=models.SET_NULL, null=True)
     liked = models.ManyToManyField(Account, null=True, blank=True, related_name='liked')
+    date_created = models.DateField(auto_now_add=True)
+    date_updated= models.DateField(auto_now=True)
 
     @property
     def num_likes(self):
--- track/models.py
+++ track/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Track(models.Model):
+    image = CloudinaryField("image")
     name = models.CharField(max_length=50)
 
     def __str__(self):
