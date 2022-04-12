7ee2828d66eaf7afe2390c2e60a1b7167f0621d2
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-12 17:26:58 +0300

    configured postgress database

--- project_tract/settings.py
+++ project_tract/settings.py
@@ -106,8 +106,10 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
