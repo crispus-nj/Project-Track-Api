729608c7650c0de7669fb1e67c70c6e6b2d1333c
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-12 13:24:19 +0300

    cleared errors

--- track/serializers.py
+++ track/serializers.py
@@ -3,11 +3,11 @@ from rest_framework import serializers
 from track.models import Track
 
 class TrackSerializer(serializers.ModelSerializer):
-    tracks = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-     )
+    # track = serializers.SlugRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     slug_field='name'
+    #  )
     class Meta:
         model = Track
         fields = "__all__"
