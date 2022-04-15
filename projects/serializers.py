2d3406e710873df26d619e659673b89f1521d1e1
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-15 12:59:35 +0300

    modified projectSerializer by adding track and track_id

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,8 +2,12 @@ from dataclasses import fields
 from pyexpat import model
 from rest_framework import serializers
 from .models import Project
+from track.serializers import TrackSerializer
 
 class ProjectSerializer(serializers.ModelSerializer):
+    track = TrackSerializer(read_only=True)
+    track_id = serializers.IntegerField()
+    
     class Meta:
         model= Project
         fields = '__all__'
